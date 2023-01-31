6db43409e4227f11f27c2b5f6d2163051a61e9ab
praveenaws7 <praveenaws7@gmail.com>
2023-01-31 19:54:22 +0530

    added terrform script for github

---
+++ github.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  token = "GithubToken"
+
+}
+resource "github_repository" "tera" {
+  name        = "terra"
+
+  visibility = "private"
+
+}
