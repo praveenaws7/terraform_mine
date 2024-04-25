9b842953a84f4a3895b0f6fbda1f118e863655d5
root <root@DESKTOP-14TPQPS>
2024-04-25 16:01:01 -0400

    added provider

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "associated_amis" {
-  value = [
-    for instance in aws_instance.eip_ec2 : {
-      ami  = instance.ami
-      id   = instance.id
-      name = instance.tags["Name"]
-    }
-  ]
-}
