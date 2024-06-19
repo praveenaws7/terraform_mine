af2c3fae382181e415922c1d659969b93f6588de
praveenaws7 <66414344+praveenaws7@users.noreply.github.com>
2024-06-18 21:43:12 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "changed_modified" {
+resource "aws_s3_bucket" "changed_modifiedagain modified" {
   bucket = "jum08th15"  # Replace with your existing bucket name
   acl    = "private"
 
